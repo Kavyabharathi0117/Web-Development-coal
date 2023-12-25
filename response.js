21d8d45723d31c2ecf718d29f6a62df996110da2
KAVYA BHARATHI G <114849447+Kavyabharathi0117@users.noreply.github.com>
2023-12-25 10:34:47 +0530

    Add files via upload

---
+++ response.js
@@ -0,0 +1,9 @@
+const responseObj = {
+  Hello:"Hello! How can I assist you today?",
+  "Tell about dgms":"Sure! DGMS,or the Directorate General of Mines Safety, is like the safety authority for coal mines in India. They make rules to keep miners safe and healthy, check that mines follow these rules, and investigate accidents to prevent future ones. Think of them as the protectors of coal miners.",
+     "Coal mine location in india": "India has mines all over the country, including in states like Jharkhand (coal), Chhattisgarh (iron ore and coal), Odisha (iron ore and more), Rajasthan (marble and minerals), Karnataka (iron ore), Goa (iron ore), and more. Each region specializes in different resources.",
+     "Indian explosive act":"The Indian Explosives Act, 1884, is a law that regulates the use, storage, and handling of explosives and fireworks in India. It's there to keep people safe and prevent unauthorized use by requiring licenses and enforcing safety rules for anyone involved with explosive materials.",
+  
+    today: new Date().toDateString(),
+    time: new Date().toLocaleTimeString(),
+  };
